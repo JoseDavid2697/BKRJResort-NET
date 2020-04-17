82dadc6910f729ba1ee8f722d04b195f823df8bd
Kenneth2698 <gera.lp98@hotmail.com>
2020-04-16 21:59:57 -0600

    Se obtienen las ofertas mediante BD, utilizando n capas

---
+++ HotelBKRJResort/Models/Business/OfertaBusiness.cs
@@ -0,0 +1,27 @@
+﻿using HotelBKRJResort.Models.Data;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelBKRJResort.Models.Business
+{
+    public class OfertaBusiness
+    {
+
+        public IConfiguration Configuration { get; }
+
+        public OfertaBusiness(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public List<Oferta> ObtenerOfertas()
+        {
+            OfertaData ofertaData = new OfertaData(this.Configuration);
+
+            return ofertaData.ObtenerOfertas();
+        }
+    }
+}
--- HotelBKRJResort/Models/Oferta.cs
+++ HotelBKRJResort/Models/Oferta.cs
@@ -7,5 +7,17 @@ namespace HotelBKRJResort.Models
 {
     public class Oferta
     {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Imagen { get; set; }
+
+        public string Link_Destino { get; set; }
+
     }
 }
+
+
